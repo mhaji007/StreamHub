ca7d645febab2d9452d7c2c4d27537dc515e98ab
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-02-27 18:10:12 -0500

    Set up custom server stylesheets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,29 +4,47 @@
 
 // Necessary modules from Next
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core";
+import React from "react";
 
 // Will only be able to render serverside props
 // using this custom document component
 class MyDocument extends Document {
   // Fetch props per document page
   static async getInitialProps(ctx) {
+    // Render the app and get the context of the page with collected side effects
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+    // Collect styles from original render on the page
+    ctx.renderPage = () =>
+      originalRenderPage({
+        envhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      });
+
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    // Pass down the props to client side
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
   }
   // Render javascript elements
   // AS document is rendered only server-side, event handlers such as
   // onClick will not work in this context
-  render () {
+  render() {
     return (
       <Html>
         <Head>
           <body>
-            <Main/>
-            <NextScript/>
+            <Main />
+            <NextScript />
           </body>
         </Head>
       </Html>
-    )
+    );
   }
 }
 
